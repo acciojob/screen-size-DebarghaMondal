246f93d11081bcc99e74f114e06f8bbb37993dd2
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-24 11:40:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function displayWindowSize(){
+	let w =window.innerWidth;
+	let h = window.innerHeight;
+	
+	document.getElementById('sizeInfo').innerHTML = "Width: " + w + ", Height: " + h;
+}
+window.addEventListener("resize", displayWindowSize);
+displayWindowSize();
+
+
